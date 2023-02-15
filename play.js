388d67fb11fd520477da7985f450e4f5741627aa
mrludovicc <nadal46@hotmail.com>
2023-02-15 15:14:09 -0500

    Creating the constants file.

--- play.js
+++ play.js
@@ -1,7 +1,7 @@
 const net = require('net');
 const { connect } = require("./client");
 const { setupInput } = require("./input");
-
+// const con = require('./constants')
 
 const conn = connect();
 setupInput(conn);
